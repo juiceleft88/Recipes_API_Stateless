4f3cc20c8e165a4a6407952d264c4ee24ad4f36a
Hugo Izquierdo <izquierdoh88@gmail.com>
2023-05-08 11:47:33 -0700

    created the logic for the search endpoint to use my custome node module for searching by recipe category. Still need to format the output for this endpoint

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,15 @@ const recipes = require('recipe_api');
 
 router.get('/', async (req, res) => {
     try{
-        console.log('This is in the / endpoint for search.js');
+        const { query, params } = req; //destructuring to get the query from the request body
+        //const { cat } = params;
+        const { category } = query; //destructuring query and assigning 'category' to it in order to use in http request "search?category="
+        const recipeCategory = await recipes.getCategory(category); //calling my custom node module and passing in the category query
+        
+        const results = recipeCategory; //showing the recipes in the specified category
+
+        console.log(req);
+        res.json(results);
     } catch (error) {
         res.status(500).json(error.toString());
     }
